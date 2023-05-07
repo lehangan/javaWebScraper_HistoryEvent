e27cc3bc8c795642226f78a89309716068936985
lehangan <lehangan30@gmail.com>
2023-05-07 15:54:51 +0700

    commit

--- web_crawl.java
+++ web_crawl.java
@@ -1,4 +1,14 @@
+//package jsoup.crawler;
+
+import org.jsoup.nodes.Document;
+import org.jsoup.Jsoup;
+import java.io.IOException;
 
 public class web_crawl {
-    
+    public static void main(String[] args) throws IOException{
+        Document doc = Jsoup.connect("https://vi.wikipedia.org/wiki/Ni%C3%AAn_bi%E1%BB%83u_l%E1%BB%8Bch_s%E1%BB%AD_Vi%E1%BB%87t_Nam").get();
+        String title = doc.title();
+        System.out.println("niên biểu lịch sử việt nam");
+    }
+
 }
